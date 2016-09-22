9ed04e036157530fb98c884d1b54ea15c277e778
Samuel Lampa <samuel.lampa@gmail.com>
2016-09-22 15:55:12 +0200

    Improve bib to md script

--- content/publication/bibtomd.py
+++ content/publication/bibtomd.py
@@ -11,20 +11,26 @@ pubs = []
 with open(args.infile) as infile:
     pub = {}
     for line in infile:
-        if line.startswith('@article'):
+        if line.startswith('@'):
             pubs.append(pub)
             pub = {}
-        m = re.match('^\s*(.*)\=(.*)$', line)
+        m = re.match('^\s*([^\=]*)\=(.*)$', line)
         if m:
-            pub[m.group(1).strip()] = m.group(2).strip(' {},').replace(r'{\"O}','Ö').replace(r'{\"A}','Ä').replace(r'{\AA}', 'Å').replace(r'{\"o}','ö').replace(r'{\"a}','ä').replace(r'{\aa}', 'å').replace(r'{\&}amp;', '&').replace(r'{\’e;}', 'é').replace(r'{\"u}', 'ü').replace(r'{\’a;}', 'á').replace(' :', ':')
+            pub[m.group(1).strip()] = m.group(2).strip(' {},').replace(r'{\"O}','Ö').replace(r'{\"A}','Ä').replace(r'{\AA}', 'Å').replace(r'{\"o}','ö').replace(r'{\"a}','ä').replace(r'{\aa}', 'å').replace(r'{\&}amp;', '&').replace(r'{\’e;}', 'é').replace(r'{\"u}', 'ü').replace(r'{\’a;}', 'á').replace(' :', ':').replace(r'\&', '&').replace('{','').replace('}','').replace('\\','')
     pubs.append(pub)
 
 for pub in pubs:
-    filename = pub['title'].lower().replace(' ', '-').replace(':','').replace('.','') + '.md'
-    print filename
-    with open(filename, 'w') as outfile:
-        outfile.write('+++\n')
-        for k, v in pub.iteritems():
-            outfile.write(k + ' = ' + v + '\n')
-        outfile.write('+++\n')
+    if not 'title' in pub:
+        print "Warning! no title in: " + str(pub)
+    else:
+        pub['title'] = pub['title'].replace('.', '')
+        filename = pub['title'].lower().replace(' ', '-').replace(':','').replace('\'','').replace('(','').replace(')','') + '.md'
+        print filename
+        with open(filename, 'w') as outfile:
+            outfile.write('+++\n')
+            for k, v in pub.iteritems():
+                if k == 'url':
+                    k = 'url_html'
+                outfile.write(k + ' = "' + v + '"\n')
+            outfile.write('+++\n\n')
 
